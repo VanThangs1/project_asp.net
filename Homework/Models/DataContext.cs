139ca447bce0bb0fb9ceffa2e2dacd29affdb048
VanThangs1 <hovanthang1201@gmail.com>
2024-10-08 13:58:36 +0700

    create project

---
+++ Homework/Models/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Homework.Areas.Admin.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Homework.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) 
+        { 
+        }
+
+        public DbSet<Menu> Menus { get; set; }
+        public DbSet<AdminMenu> AdminMenus { get; set; }
+    }
+}
---
+++ Homework/Program.cs
@@ -0,0 +1,42 @@
+using Homework.Models;
+using Microsoft.EntityFrameworkCore;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(connection));
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    _ = endpoints.MapControllerRoute(
+      name: "areas",
+      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    );
+});
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
